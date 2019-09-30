affce5bb5c8f70aa3ccc02be988c9600c8a9085b
Katherine <RocKa840@school.lu>
2019-09-30 10:55:43 +0200

    ???

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
-alert a = prompt("What is your age?")
-alert b = prompt("Do you have a diploma?")
+alert a = prompt("What is your age?");
+alert b = prompt("Do you have a diploma?");
 
-if (a > 20) {
-
-}
+if (a > 20); {
+    if (b == "yes") { alert "You can work" }
+};
